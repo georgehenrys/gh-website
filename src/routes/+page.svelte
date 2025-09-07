48efd0431211940c9ef5cdfdee224443a6a64ece
Ross Root <git@rroot.me>
2025-09-06 21:14:31 -0700

    chore: dailyui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
+<script>
+  import { Phone } from '@lucide/svelte';
+</script>
+
+<div class="navbar bg-base-100 shadow-sm">
+  <div class="flex-1">
+    <a class="btn btn-ghost text-xl">daisyUI</a>
+  </div>
+  <div class="navbar-end gap-3">
+    <Phone />
+    (928) 474-5037
+    <a class="btn btn-error">Schedule Now</a>
+  </div>
+</div>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
